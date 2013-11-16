65faaadfb8af6a2e8d79626532ca053f3ecbe11c
funnythingz <hiroki@hackleberry.me>
2013-11-16 15:13:20 +0900

    added iron-router

--- src/reference.ts
+++ src/reference.ts
@@ -1,6 +1,5 @@
 /// <reference path='../definitions/meteor.d.ts'/>
 /// <reference path='../definitions/jquery.d.ts'/>
-/// <reference path='../definitions/backbone.d.ts'/>
 /// <reference path='view-model-types.ts'/>
 
 /// <reference path="ddd/base/entity-interface.ts" />
